44d046de420aeaa42b5a2b269f326b672c0387d9
Anish Kumar <anishkumargvm02@gmail.com>
2024-09-11 10:54:25 +0530

    revisison loops for: of, in

--- Section_1/JavaScript/loops.js
+++ Section_1/JavaScript/loops.js
@@ -1,17 +1,76 @@
-// for(var i=0;i<=9;i++){
-//     console.log(i);
+// for(var i=1;i<=20;i++){
+//     if(i%2==0) console.log(i)
 // }
 
 
-// var i=1;
-// while(i<=10){
+// let i=1;
+// while(i<=20){
+//     if(1%2==0) console.log(i)
+//     i++
+// }
+
+
+
+
+//for of loop
+// let names = ["John", "Smith", "Sam"]
+// for(let name of names) console.log(name, "is a good human")
+
+
+
+//for in loop
+// let person = {
+//     name: "john",
+//     age: 30,
+//     city: "New York"
+// }
+// for(let k in person){
+//     console.log(k,":",person[k])
+// }
+
+
+
+// let i = 20;
+// do {
 //     console.log(i);
 //     i++;
+// } while (i<=14);
+
+
+
+
+// let sum =0                      
+// for(var i=1;i<=10;i++){
+//     sum+=i
 // }
+// console.log(sum)
 
 
-let i = 20;
-do {
-    console.log(i);
-    i++;
-} while (i<=14);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
